f256f67832f0e01c7f1ffc2259a34b69261a6958
I-m-Eden <fangyoule@gmail.com>
2018-11-17 15:24:50 +0800

    添加项目文件。

---
+++ SAO/source.cpp
@@ -0,0 +1,67 @@
+﻿//
+//	source.cpp
+//	SAO
+//
+//	Created by Eden on 17/11/18.
+//	Copyright © 2018年 author. All rights reserved.
+//
+//
+//	用Visual Studio 2017编译之前注意事项：
+//	项目属性->c/c++->预处理器->预处理器定义 中添加：
+//	_CRT_SECURE_NO_DEPRECATE
+//	_CRT_NONSTDC_NO_DEPRECATE
+//	项目属性->c/c++->代码生成->运行库 修改为 "多线程调试 (/MTd)"
+//
+
+//#define DEBUGGING
+
+#define _WIN32_WINNT _WIN32_WINNT_MAXVER
+#include "head.h"
+#include "vector2.h"
+#include "datastruct.h"
+#include "winpaint.h"
+#include "toolibrary.h"
+#include "coloresource.h"
+#include "imageresource.h"
+#include "processresource.h"
+using namespace std;
+#define ref(i,x,y)for(int i=(x);i<=(y);++i)
+#define def(i,x,y)for(int i=(x);i>=(y);--i)
+const double pi = acos(-1);
+const double eps = 1e-5;
+string constr(int s) {
+	string res = "";
+	if (s < 0)res = "-" + constr(-s); else
+		if (s < 10)res = res + char(s + 48); else
+			res = constr(s / 10), res = res + char(s % 10 + 48);
+	return res;
+}
+void flushmouse() { while (GetAsyncKeyState(VK_LBUTTON) & 0x8000)delay(1); while (peekmsg())delay(1); }
+void flushkey() { FlushConsoleInputBuffer(GetStdHandle(STD_INPUT_HANDLE)); }
+
+namespace showtitle {
+	void Main() {
+	}
+}
+
+void Main() {
+	showtitle::Main();
+}
+
+int WINAPI WinMain(
+	HINSTANCE hInstance,
+	HINSTANCE hPrevinstance,
+	LPSTR lpCmdline,
+	int nCmdshow
+) {
+
+	if (checkprev("SAO"))return 0;
+	initwin(hInstance, 800, 600, "SAO");
+	showwin(nCmdshow);
+
+	srand((UINT)time(0));
+	Main();
+
+	closewin(hwnd);
+	return 0;
+}
